d21e60324dd5e67d6d9c3e48e0f2249ed83da782
DevinBayly <baylyd@email.arizona.edu>
2020-03-28 16:25:27 -0700

    added leaflet material to page

---
+++ leaflet.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    let mapContainer = document.querySelector("#mapcontainer")
+    mapContainer.style.height = "300px"
+// make the map
+let map = L.map("mapcontainer", {
+  center: [32.253460, -110.911789], // latitude, longitude in decimal degrees (find it on Google Maps with a right click!)
+  zoom:10, // can be 0-22, higher is closer
+  scrollWheelZoom: true // don't zoom the map on scroll
+});
+// add the basemap tiles
+L.tileLayer(
+  "https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}@2x.png" // stamen toner tiles
+  // stamen toner tiles
+).addTo(map);
+
+}
