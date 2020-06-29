daf1dd76fcdb8d538e9bc196d404603ec33ef3e8
Alex Bigelow <alex.r.bigelow@gmail.com>
2020-06-29 06:58:54 -0700

    Update all dependencies in package.json

--- views/Footer/Footer.js
+++ views/Footer/Footer.js
@@ -2,10 +2,13 @@ import { View } from '../../node_modules/uki/dist/uki.esm.js';
 
 class Footer extends View {
   constructor (d3el) {
-    super(d3el, [
-      { type: 'text', url: 'views/Footer/template.html' },
-      { type: 'less', url: 'views/Footer/style.less' }
-    ]);
+    super({
+      d3el,
+      resources: [
+        { type: 'text', url: 'views/Footer/template.html' },
+        { type: 'less', url: 'views/Footer/style.less' }
+      ]
+    });
   }
   setup () {
     this.d3el.html(this.resources[0])
--- views/StatusWidget/StatusWidget.js
+++ views/StatusWidget/StatusWidget.js
@@ -1,17 +1,19 @@
-/* globals d3 */
 import { View } from '../../node_modules/uki/dist/uki.esm.js';
 
 class StatusWidget extends View {
   constructor (d3el) {
-    super(d3el, [
-      { type: 'text', url: 'views/StatusWidget/template.html' },
-      { type: 'text', url: 'views/StatusWidget/stage0.html' },
-      { type: 'text', url: 'views/StatusWidget/stage1.html' },
-      { type: 'text', url: 'views/StatusWidget/stage2.html' },
-      { type: 'text', url: 'views/StatusWidget/stage3.html' },
-      { type: 'text', url: 'views/StatusWidget/stage4.html' },
-      { type: 'less', url: 'views/StatusWidget/style.less' }
-    ]);
+    super({
+      d3el,
+      resources: [
+        { type: 'text', url: 'views/StatusWidget/template.html' },
+        { type: 'text', url: 'views/StatusWidget/stage0.html' },
+        { type: 'text', url: 'views/StatusWidget/stage1.html' },
+        { type: 'text', url: 'views/StatusWidget/stage2.html' },
+        { type: 'text', url: 'views/StatusWidget/stage3.html' },
+        { type: 'text', url: 'views/StatusWidget/stage4.html' },
+        { type: 'less', url: 'views/StatusWidget/style.less' }
+      ]
+    });
   }
   get myStage () {
     return parseInt(window.localStorage.getItem('stage') || '0');
